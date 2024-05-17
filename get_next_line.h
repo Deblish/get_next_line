a05c7f84f2c47040dec964638c8b41de49871a05
Aaron Padilla <aapadill@student.hive.fi>
2024-05-17 15:10:56 +0300

    start again

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 18:10:10 by aapadill          #+#    #+#             */
+/*   Updated: 2024/05/16 18:20:32 by aapadill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*get_next_line(int fd);
+#endif
