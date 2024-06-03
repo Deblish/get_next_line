6fd9644bcce077e208ae43751175384c4409d31e
Aaron Padilla <aapadill@c3r3p1.hive.fi>
2024-06-03 15:20:27 +0300

    freeing something unfreiable

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,42 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 18:06:36 by aapadill          #+#    #+#             */
-/*   Updated: 2024/06/03 12:22:06 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/06/03 15:19:17 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE 22
+#define BUFFER_SIZE 2
 
 //remove
 #include <fcntl.h>
 #include <stdio.h>
+
+static char	*ft_update_buffer(char *buffer)
+{
+	char	*eol;
+	char	*start;
+	char	*new;
+
+	eol = ft_strchr(buffer, '\n');
+	start = buffer;
+	if (!eol)
+		start = eol + 1;
+	if (!*start)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	new = malloc(ft_strlen(start) + sizeof(char));
+	if (!new)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	ft_strlcpy(new, start, ft_strlen(start) + sizeof(char));
+	free(buffer);
+	return (new);
+}
 
 static char	*ft_get_line(char *buffer, char *line)
 {
@@ -29,7 +55,10 @@ static char	*ft_get_line(char *buffer, char *line)
 		len = eol - buffer + 1;
 	line = malloc(len + sizeof(char)); 
 	if (!line)
-		return (NULL); //free buffer
+	{
+		free(buffer);
+		return (NULL);
+	}
 	ft_strlcpy(line, buffer, len + sizeof(char));
 	return (line);
 }
@@ -41,20 +70,29 @@ static char	*ft_read(int fd, char *buffer)
 
 	bytes_saved = malloc(BUFFER_SIZE + sizeof(char));
 	if (!bytes_saved)
-		return (NULL); //free buffer
+	{
+		free(buffer);
+		return (NULL);
+	}
 	//check for '\n' and erase them?
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, bytes_saved, BUFFER_SIZE);
 		if (bytes_read < 0)
-			return (NULL); //free bytes_saved
+		{
+			free(bytes_saved);
+			return (NULL);
+		}
 		if (bytes_read == 0)
 			break ;
 		bytes_saved[bytes_read] = 0;
 		buffer = ft_strjoin(buffer, bytes_saved);
 		if (!buffer)
-			return (NULL); //free bytes_saved
+		{
+			free(bytes_saved);
+			return (NULL);
+		}
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
@@ -68,13 +106,19 @@ char	*get_next_line(int fd)
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || 0 > read(fd, 0, 0))
-		return (NULL); //clean buffer
+	{
+		free(buffer);
+		return (NULL);
+	}
 	buffer = ft_read(fd, buffer);
-	//if (!buffer)
-		//free buffer?
+	//{
+	//	free(buffer);
+	//	return (NULL);
+	//}
+	//free buffer?
 	ft_get_line(buffer, line); //if null clean buffer
-	//update buffer
-	return (buffer);
+	ft_update_buffer(buffer);
+	return (buffer); //if null what?
 }
 
 int	ft_open(char *file)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 18:10:18 by aapadill          #+#    #+#             */
-/*   Updated: 2024/06/03 11:40:14 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/06/03 15:11:19 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,44 +26,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	*dst = 0;
 	return (srclen);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	len;
-	char	*dest;
-	char	*result;
-
-	len = ft_strlen(s1);
-	dest = malloc((len + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
-	result = dest;
-	while (*s1)
-		*dest++ = *s1++;
-	*dest = 0;
-	return (result);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*sub;
-	size_t	s_len;
-	size_t	writable;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (ft_strdup(""));
-	writable = len;
-	if (s_len - start < len)
-		writable = s_len - start;
-	sub = malloc(writable + sizeof(char));
-	if (!sub)
-		return (NULL);
-	ft_strlcpy(sub, s + start, writable + 1);
-	return (sub);
 }
 
 size_t	ft_strlen(const char *s)
@@ -86,7 +48,11 @@ char	*ft_strjoin(char *s1, char *s2)
 	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	//clean possible '/n's
 	if (!str)
-		return (NULL); //free s1
+	{
+		if (s1)
+			free(s1);
+		return (NULL);
+	}
 	joined = str;
 	while (s1 && *s1)
 		*str++ = *s1++;
